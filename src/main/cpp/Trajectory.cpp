2a36f1ba21b0d94b7244815c1effb2f06509c3ba
Srikar Andra <67433991+s-andra21@users.noreply.github.com>
2025-01-22 17:46:23 -0800

    debugged trajectory.cpp and trajectory.h

---
+++ src/main/cpp/Trajectory.cpp
@@ -0,0 +1,107 @@
+#include "Trajectory.h"
+#include "SwerveDrive.h"
+#include "SwerveModule.h"
+#include "Robot.h"
+
+#include <pathplanner/lib/trajectory/PathPlannerTrajectory.h>
+#include <pathplanner/lib/path/PathPlannerPath.h>
+#include <pathplanner/lib/path/GoalEndState.h>
+
+#include <frc/controller/HolonomicDriveController.h>
+#include <frc/kinematics/ChassisSpeeds.h>
+
+// controller used to track trajectories + correct minor disturbances
+static frc::HolonomicDriveController controller{
+    frc::PIDController{0.1, 0, 0},
+    frc::PIDController{0.1, 0, 0},
+    frc::ProfiledPIDController<units::radian>{
+        steerP, 0, 0,
+        frc::TrapezoidProfile<units::radian>::Constraints{
+            units::radians_per_second_t(189.2),
+            units::radians_per_second_squared_t(2665.993 * (25.8 / 7.6))}}};
+
+/**
+ * Drives robot to the next state on trajectory
+ * Odometry must be in meters
+ */
+void Trajectory::driveToState(PathPlannerTrajectoryState const &state)
+{
+    frc::ChassisSpeeds const correction = controller.Calculate(mDrive.getOdometryPose(), state.pose, state.linearVelocity, state.deltaRot);
+
+    double vx_feet = correction.vx.value() * 3.281;
+    double vy_feet = correction.vy.value() * 3.281;
+    double rotCompass = Rotation2d::compassToPolar(state.deltaRot.Radians().value());
+
+    mDrive.Drive(ChassisSpeeds{-vy_feet, vx_feet, correction.omega.value()}, Rotation2d{rotCompass}, false);
+}
+
+/**
+ * Follows pathplanner trajectory
+ */
+void Trajectory::follow(std::string const &traj_dir_file_path)
+{
+    auto path = PathPlannerPath::fromPathFile(traj_dir_file_path);
+    PathPlannerTrajectory traj = PathPlannerTrajectory(path,  frc::ChassisSpeeds(), frc::Rotation2d(0_rad), config);
+
+    auto const initialState = traj.getInitialState();
+    auto const initialPose = initialState.pose;
+    mDrive.resetOdometry(initialPose.Translation(), initialState.heading);
+
+    frc::Timer trajTimer;
+    trajTimer.Start();
+
+    while ((mDrive.state == DriveState::Auto) && (trajTimer.Get() <= traj.getTotalTime()))
+    {
+        auto currentTime = trajTimer.Get();
+        auto sample = traj.sample(currentTime);
+
+        driveToState(sample);
+        mDrive.updateOdometry();
+
+        frc::SmartDashboard::PutNumber("curr pose x meters", mDrive.getOdometryPose().Translation().X().value());
+        frc::SmartDashboard::PutNumber("curr pose y meters", mDrive.getOdometryPose().Translation().Y().value());
+
+        using namespace std::chrono_literals;
+
+        // refresh rate of holonomic drive controller's PID controllers (edit if needed)
+        std::this_thread::sleep_for(20ms);
+    }
+    mDrive.stopModules();
+}
+
+/**
+ * Calls sequences of follow functions for set paths
+ */
+void Trajectory::followPath(int numPath)
+{
+    switch (numPath)
+    {
+    // do nothing
+    case 0: 
+        break; 
+
+    // straight
+    case 1:
+        follow("Straight");
+        break;
+
+    // straight, shoot
+    case 2:
+        follow("Straight");
+        //  CALL SUPERSTRUCTURE SHOOT SPEAKER FOR SHOOTER AUTO
+        break;
+
+    // straight, shoot, straight
+    case 3:
+        follow("Straight");
+        break;
+
+    // curve
+    case 4:
+        follow("Curve");
+        break;
+
+    default:
+        break;
+    }
+}
---
+++ src/main/include/Trajectory.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "SwerveDrive.h"
+#include "Constants.h"
+#include "sensors/Limelight.h"
+#include "geometry/Translation2d.h"
+
+#include <pathplanner/lib/trajectory/PathPlannerTrajectory.h>
+#include <pathplanner/lib/path/PathPlannerPath.h>
+#include <pathplanner/lib/path/GoalEndState.h>
+
+#include "frc/geometry/Rotation2d.h"
+#include "frc/geometry/Pose2d.h"
+
+#include "units/velocity.h"
+#include "units/angle.h"
+#include "units/angular_acceleration.h"
+#include "units/angular_velocity.h"
+
+#include <chrono>
+#include <frc/Timer.h>
+
+using namespace pathplanner;
+
+class Trajectory
+{
+private:
+    SwerveDrive &mDrive;
+    RobotConfig &config;
+
+public:
+    // Trajectory(SwerveDrive &mDriveInput) : mDrive(mDriveInput) {};
+
+    void driveToState(PathPlannerTrajectoryState const &state);
+
+    void follow(std::string const &traj_dir_file_path);
+
+    void followPath(int numPath);
+
+    void testHolonomic(frc::Pose2d const &target_pose,
+                       units::velocity::meters_per_second_t const &velocity,
+                       frc::Rotation2d const &target_rot);
+};
